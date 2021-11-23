cee5538617ae55bd5afce8e4dd11c043b2caecf3
Mohamed Said <themohamedsaid@gmail.com>
2021-11-23 19:19:39 +0200

    wip

--- pages/profile.js
+++ pages/profile.js
@@ -1,6 +1,39 @@
 import Head from 'next/head'
 import Link from 'next/link'
 
+let reservations = [
+    {
+        "office": {
+            "images": [
+                {
+                    "path": "https://via.placeholder.com/400x400.png?text=PLACEHOLDER",
+                }
+            ],
+            "id": 1,
+            "title": "Office One",
+            "description": "Architecto assumenda cum eum. Voluptas qui dignissimos qui voluptate. Mollitia necessitatibus ut sit. Et saepe ea quo nulla.",
+        },
+        "start_date": "2021-10-23T17:03:41",
+        "end_date": "2021-11-12T17:03:41",
+        "price": 12000,
+    },
+    {
+        "office": {
+            "images": [
+                {
+                    "path": "https://via.placeholder.com/400x400.png?text=PLACEHOLDER",
+                }
+            ],
+            "id": 2,
+            "title": "Office Wto",
+            "description": "Quia voluptatem amet quo minus. Repudiandae sed beatae veritatis. Error quos quia qui pariatur perferendis beatae occaecati.",
+        },
+        "start_date": "2021-08-23T17:03:41",
+        "end_date": "2021-09-30T17:03:41",
+        "price": 19000,
+    }
+];
+
 export default function Profile() {
 
     return (
@@ -17,63 +50,32 @@ export default function Profile() {
                 <span>Here is a list of your previous reservations!</span>
             </div>
 
-            <div className="flex pb-10 mb-10 border-b">
-                <div className="w-1/3 h-56 relative overflow-hidden rounded-lg">
-                    <img src="https://via.placeholder.com/400x400.png?text=PLACEHOLDER" className="object-cover w-full h-full"></img>
-                </div>
-
-                <div className="w-full pl-14">
-                    <div className="flex justify-between items-center mb-6">
-                        <h1 className="text-2xl font-bold">Large office space</h1>
-                        <span className="block font-semibold">$12 per day</span>
+            {reservations.map((reservation, index) => (
+                <div key={reservation.office.id} className={`flex ${index + 1 == reservations.length ? '' : 'pb-10 mb-10 border-b'}`}>
+                    <div className="w-1/3 h-56 relative overflow-hidden rounded-lg">
+                        <img src={reservation.office.images[0].path} className="object-cover w-full h-full"></img>
                     </div>
-                    <p className="leading-loose mb-5">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aperiam delectus doloremque doloribus eius eligendi molestiae molestias nihil nostrum nulla obcaecati omnis, quia quidem sequi sunt tempora temporibus vero voluptatum?
-                    </p>
-                    <Link href={"/offices/" + 1}>
-                        <a className="text-purple-600 font-bold">More details...</a>
-                    </Link>
-                </div>
-            </div>
 
-            <div className="flex pb-10 mb-10 border-b">
-                <div className="w-1/3 h-56 relative overflow-hidden rounded-lg">
-                    <img src="https://via.placeholder.com/400x400.png?text=PLACEHOLDER" className="object-cover w-full h-full"></img>
-                </div>
 
-                <div className="w-full pl-14">
-                    <div className="flex justify-between items-center mb-6">
-                        <h1 className="text-2xl font-bold">Large office space</h1>
-                        <span className="block font-semibold">$12 per day</span>
+                    <div className="w-full pl-14">
+                        <div className="flex justify-between items-center mb-6">
+                            <h1 className="text-2xl font-bold">{reservation.office.title}</h1>
+                            <span className="block font-semibold">Total ${reservation.price / 100}</span>
+                        </div>
+                        <p className="mb-5">
+                                                <span className="text-gray-600 text-sm uppercase">
+                                                    From <strong>{reservation.start_date.split('T')[0]}</strong> To <strong>{reservation.end_date.split('T')[0]}</strong>
+                                                </span>
+                        </p>
+                        <p className="leading-loose mb-5">
+                            {reservation.office.description}
+                        </p>
+                        <Link href={"/offices/" + reservation.office.id}>
+                            <a className="text-purple-600 font-bold">More details...</a>
+                        </Link>
                     </div>
-                    <p className="leading-loose mb-5">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aperiam delectus doloremque doloribus eius eligendi molestiae molestias nihil nostrum nulla obcaecati omnis, quia quidem sequi sunt tempora temporibus vero voluptatum?
-                    </p>
-                    <Link href={"/offices/" + 1}>
-                        <a className="text-purple-600 font-bold">More details...</a>
-                    </Link>
                 </div>
-            </div>
-
-            <div className="flex">
-                <div className="w-1/3 h-56 relative overflow-hidden rounded-lg">
-                    <img src="https://via.placeholder.com/400x400.png?text=PLACEHOLDER" className="object-cover w-full h-full"></img>
-                </div>
-
-
-                <div className="w-full pl-14">
-                    <div className="flex justify-between items-center mb-6">
-                        <h1 className="text-2xl font-bold">Large office space</h1>
-                        <span className="block font-semibold">$12 per day</span>
-                    </div>
-                    <p className="leading-loose mb-5">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aperiam delectus doloremque doloribus eius eligendi molestiae molestias nihil nostrum nulla obcaecati omnis, quia quidem sequi sunt tempora temporibus vero voluptatum?
-                    </p>
-                    <Link href={"/offices/" + 1}>
-                        <a className="text-purple-600 font-bold">More details...</a>
-                    </Link>
-                </div>
-            </div>
+            ))}
         </>
     )
 }
